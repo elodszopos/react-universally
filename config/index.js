0213d8f1900a4b2101d1fea9292698d9229af1f1
Elod Szopos <elodszopos@hotmail.com>
2016-12-28 09:09:34 +0200

    favico fix

--- config/index.js
+++ config/index.js
@@ -110,29 +110,6 @@ const config = {
       },
     ],
     links: [
-      // http://realfavicongenerator.net/
-      {
-        rel: 'apple-touch-icon',
-        sizes: '180x180',
-        href: '/apple-touch-icon.png',
-      },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        href: '/favicon-32x32.png',
-        sizes: '32x32',
-      },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        href: '/favicon-16x16.png',
-        sizes: '16x16',
-      },
-      {
-        rel: 'mask-icon',
-        href: '/safari-pinned-tab.svg',
-        color: '#00a9d9',
-      },
       {
         rel: 'manifest',
         href: '/manifest.json',
